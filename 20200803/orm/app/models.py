c0bb33ac9731698c0246fbfd3a8aee512d4201b4
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-03 14:46:33 +0100

    Update

--- 20200803/orm/app/models.py
+++ 20200803/orm/app/models.py
@@ -11,3 +11,11 @@ def addProduct(code, name, price):
 
 def getProductList():
     return Product.objects.all()        
+
+if getProductList().count() == 0:
+    addProduct('IPX', 'IPhone X', 17.5)
+    addProduct('IP8', 'IPhone 8', 12.5)
+    addProduct('IP7', 'IPhone 7', 8.5)
+
+for p in getProductList():
+    print(p.code, p.name, p.price)    
