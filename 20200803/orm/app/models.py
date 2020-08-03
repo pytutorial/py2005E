6b25ac202c77b468c2fa5af809f8bd37d51f1e52
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-03 15:18:26 +0100

    Update

--- 20200803/orm/app/models.py
+++ 20200803/orm/app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
+class Category(models.Model):
+    code = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=200)
+
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True)
     code = models.CharField(max_length=30, unique=True)
     name = models.CharField(max_length=200)
     price = models.FloatField()
@@ -15,7 +20,4 @@ def getProductList():
 if getProductList().count() == 0:
     addProduct('IPX', 'IPhone X', 17.5)
     addProduct('IP8', 'IPhone 8', 12.5)
-    addProduct('IP7', 'IPhone 7', 8.5)
-
-for p in getProductList():
-    print(p.code, p.name, p.price)    
+    addProduct('IP7', 'IPhone 7', 8.5) 
