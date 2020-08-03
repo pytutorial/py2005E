dd062d3caac13d3f0127229bd0fed5a9eeb7c7d9
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-03 12:57:06 +0100

    Update

--- 20200803/webservice/app/views.py
+++ 20200803/webservice/app/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import json
 
 def index(request):
     return render(request, 'index.html')
+
+def hello(request): #127.0.0.1:8000/hello
+    content = json.dumps({'message': 'hello'})
+    return HttpResponse(content, content_type='application/json')    
