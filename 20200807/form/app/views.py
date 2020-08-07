34f67e49be97905f80c4c9e796dc1570fc89e7aa
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-07 13:10:08 +0100

    Update

--- 20200807/form/app/views.py
+++ 20200807/form/app/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
-
+from .forms import *
 def index(request):
-    return render(request, 'index.html')
+    form = CustomerForm()
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            return render('thankyou.html')
+    context = {'form': form}
+    return render(request, 'index.html', context)
