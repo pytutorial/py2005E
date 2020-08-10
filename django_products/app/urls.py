d33616a6cfbbc0b685339f52c0fa953dc446cd3c
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-10 13:05:55 +0100

    Update

--- django_products/app/urls.py
+++ django_products/app/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns =[
     path('', index, name='home'),    
     path('add_category', addCategory, name='add-category'),
+    path('update_category/<pk>', updateCategory, name='update-category'),
+    path('delete_category/<pk>', deleteCategory, name='delete-category'),
+
 ]
--- django_products/app/views.py
+++ django_products/app/views.py
@@ -15,3 +15,22 @@ def addCategory(request):
             return redirect('home')
 
     return render(request, 'category/form.html', {'form': form})
+
+def updateCategory(request, pk):
+    category = Category.objects.get(pk=pk)
+    form = CategoryForm(instance=category)
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    return render(request, 'category/form.html', {'form': form})
+
+
+def deleteCategory(request, pk):
+    category = Category.objects.get(pk=pk)
+    category.delete()
+    return redirect('home')
+
