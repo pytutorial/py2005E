04a0acd09b4b19f633432786ae13eeba3aaf4057
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-17 12:58:30 +0100

    Update

--- 20200817/user_login/app/urls.py
+++ 20200817/user_login/app/urls.py
@@ -1,7 +1,8 @@
 #app/urls.py
 from django.urls import path
 from .views import *
-
+from django.urls import include
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', index),
 ]
